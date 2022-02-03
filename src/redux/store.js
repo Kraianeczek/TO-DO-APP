541b2062ad53e382be0948f21118b171caa9b3a4
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-03 16:33:42 +0100

    wadd cards

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { createStore } from 'redux';
+import shortid from 'shortid';
 import initialState from './initialState';
 
 const reducer = (state, action) => {
-  if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.newColumn]}
-  return state;
+  switch (action.type) {
+    case'ADD_COLUMN': return { ...state, columns: [...state.columns, {...action.payload, id: shortid() }]};
+    case'ADD_CARD': return { ...state, cards: [...state.cards, {...action.payload }]};
+    default:
+      return state;
+  }
 };
 
 const store = createStore(

dea401f188661b1d8567f84a4ae54fa6314ea9d3
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-04 21:02:58 +0100

    check it

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,7 +5,10 @@ import styles from './Column.module.scss'
 
 const Column = (props) => {
 
-    const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id)
+    const searchString = useSelector(state => state.searchString);
+    console.log('ss', searchString);
+    const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase()))
+
     return (
         <article className={styles.column}>
             <h1 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h1>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import Button from '../Button/Button.js';
 import TextInput from '../TextInput/TextInput.js';
-import styles from './SearchForm.module.scss'
+import styles from './SearchForm.module.scss';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
 
+    const dispatch = useDispatch();
     const [title, setTitle] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
+        console.log('titsss', title)
+        dispatch({ type: 'FILTER', payload: {title}})
         setTitle('');
+        console.log('tit', title)
     };
 
-    console.log('tit', title)
     return (
         <form className={styles.searchForm} onSubmit={handleSubmit}>
             <TextInput placeholder="Search..." value={title} onChange={e => setTitle(e.target.value)} />

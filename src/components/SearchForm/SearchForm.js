417259ce0557ca2b77834ba685325c17544e8046
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-04 20:03:46 +0100

    wadd preventDefault on search button

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,8 +3,13 @@ import TextInput from '../TextInput/TextInput.js';
 import styles from './SearchForm.module.scss'
 
 const SearchForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+
     return (
-        <form className={styles.searchForm}>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
             <TextInput placeholder="Search..."/>
             <Button>
                 <span className="fa fa-search" />

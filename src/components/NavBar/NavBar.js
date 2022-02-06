c60a4898bf2b7038abce380b3fc707db71b1e263
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-06 20:45:40 +0100

    add color-warning to links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import styles from './NavBar.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return(
@@ -8,9 +8,9 @@ const NavBar = () => {
                 <h3 className={styles.span}><span className="fa fa-tasks"></span></h3> 
             </span> 
             <span>
-                <h3 className={styles.home}><Link to="/">Home</Link></h3>
-                <h3 className={styles.favorite}><Link to="/favorite">Favorite</Link></h3>
-                <h3 className={styles.about}><Link to="/about">About</Link></h3>
+                <h3 className={styles.home}><NavLink className={({ isActive }) => isActive && styles.linkActive} to="/">Home</NavLink></h3>
+                <h3 className={styles.favorite}><NavLink className={({ isActive }) => isActive && styles.linkActive} to="/favorite">Favorite</NavLink></h3>
+                <h3 className={styles.about}><NavLink className={({ isActive }) => isActive && styles.linkActive} to="/about">About</NavLink></h3>
             </span>
         </container>
     )

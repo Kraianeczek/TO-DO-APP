b3ce441ee60a850e65655ae029fdd9e3b5252002
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-07 12:25:17 +0100

    wadd imports title & description

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -4,16 +4,25 @@ const initialState = {
         id: 1,
         title: 'Books',
         icon: 'book',
+        listId: 1,
       },
       {
         id: 2,
         title: 'Movies',
         icon: 'film',
+        listId: 1,
       },
       {
         id: 3,
         title: 'Games',
         icon: 'gamepad',
+        listId: 1,
+      },
+      {
+        id: 4,
+        title: 'Tests',
+        icon: 'music',
+        listId: 2,
       }
     ],
   
@@ -26,7 +35,20 @@ const initialState = {
       { id: 6, columnId: 3, title: 'Skyrim' }
     ],
 
-    searchString: ''
+    searchString: '',
+
+    lists: [
+      {
+        id: 1,
+        title: 'Things to do...',
+        description: 'Interesting things I want to check out'
+      },
+     {
+        id: 2,
+        title: 'Test list',
+        description: 'Lorem Ipsum'
+     }
+   ]
   
   };
   

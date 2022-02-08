cfd9acbfad085b813f60574937b678c11af33075
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-08 13:28:10 +0100

    wadd adding lists

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addList } from "../../redux/store";
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import styles from './ListForm.module.scss';
+
+
+
+const ListForm = () => {
+
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({ title, description }));
+        setTitle('');
+        setDescription('');
+    }
+
+    return (
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <span className={styles.span}>Title:</span><TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <span className={styles.span}>Description:</span><TextInput type="text" value={description} onChange={e => setDescription(e.target.value)} />
+            <Button>Add List</Button>
+        </form>
+    )
+}
+
+export default ListForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -15,12 +15,14 @@ export const getAllLists = (state => state.lists);
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const filter = payload => ({ type: 'FILTER', payload });
+export const addList = payload => ({ type: 'ADD_LIST', payload });
 
 const reducer = (state, action) => {
   switch (action.type) {
     case'ADD_COLUMN': return { ...state, columns: [...state.columns, {...action.payload, id: shortid() }]};
     case'ADD_CARD': return { ...state, cards: [...state.cards, {...action.payload, id: shortid() }]};
     case 'FILTER': return { ...state, searchString: action.payload.title};
+    case 'ADD_LIST': return { ...state, lists: [...state.lists, {...action.payload, id: shortid()}]}
     default:
       return state;
   }
